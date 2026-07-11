4a7be565f6478175a4093348530501826199bd27
Banagiri Yashasvi <23eg105b05@anurag.edu.in>
2026-07-11 10:14:54 +0530

    Delete crafttrail directory

--- crafttrail/server/src/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-export async function connectDB(uri) {
-  mongoose.set('strictQuery', true);
-  await mongoose.connect(uri);
-  console.log(`[db] connected -> ${mongoose.connection.name}`);
-  return mongoose.connection;
-}
